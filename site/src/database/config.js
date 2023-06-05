93beb0b6b9075ded1488e2fcc92fba0f4527369e
eduufreire <eduardo.freire@sptech.school>
2023-06-05 13:28:47 -0300

    Ajustes

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "mineGuide",
-    user: "root",
-    password: "@eduufreire",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
